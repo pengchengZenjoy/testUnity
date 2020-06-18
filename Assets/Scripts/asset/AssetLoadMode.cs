7d72c25563e2199daaa8028d339c5bae42bbc2b9
pengchengZenjoy <cheng.peng@zenjoy.net>
2020-06-18 15:32:28 +0800

    first

---
+++ Assets/Scripts/asset/AssetLoadMode.cs
@@ -0,0 +1,4 @@
+﻿public enum AssetLoadMode{
+    Editor,    //编辑器模式(该模式直接加载资源)
+    AssetBundler,    //ab包模式(该模式所有资源使用ab方式加载)
+}
---
+++ Assets/Scripts/asset/IAssetLoader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine.Events;
+
+public interface IAssetLoader{
+    //异步加载
+    IEnumerator LoadAssetAsync<T>(string path, UnityAction<T> callback) where T : class;
+    //同步加载
+    T LoadAsset<T>(string path) where T : class;
+}
